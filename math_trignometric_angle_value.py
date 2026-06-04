3de69fa28bda69652040e2c47e2f49bdcd49648e
sanath10324 <sanathkatkamgamer@gmail.com>
2026-06-04 09:11:27 +0400

    Math Trignometric Value

---
+++ math_trignometric_angle_value.py
@@ -0,0 +1,9 @@
+import math
+
+angle = float(input("Enter an angle in degrees: "))
+
+radians = math.radians(angle)
+
+print("Sin =", math.sin(radians))
+print("Cos =", math.cos(radians))
+print("Tan =", math.tan(radians))
