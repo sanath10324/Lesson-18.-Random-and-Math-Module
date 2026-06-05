1ed143961205466f4418ee1ce0278c99a0bef026
sanath10324 <sanathkatkamgamer@gmail.com>
2026-06-05 09:00:25 +0400

    Month Names

--- python_test2.py
+++ python_test2.py
@@ -1,31 +1,44 @@
 def add(a,b):
     return(a+b)
 
-
 def subtract(a,b):
     return(a-b)
 
 def multiply(a,b):
     return(a*b)
 
-def division(a,b):
+def divide(a,b):
     return(a/b)
 
-a = float(input("Enter a numebr (a): "))
-b = float(input("Enter a number (b): "))
-operation = str(input("Enter a operation [+, -, *, /]: "))
+try:
+    num1 = float(input("Enter num1:"))
+    num2 = float(input("Enter num2:"))
 
-if operation == ("+"):
-    add
-    print("The answer is:", add)
-elif operation == ("-"):
-    subtract
-    print("The answer is:", subtract)
-elif operation == ("*"):
-    multiply
-    print("The answer is:", multiply)
-elif operation == ("/"):
-    division
-    print("The answer is:", division)
-else:
-    print("ERROR!")
+    print("\nChoose a Operation: ")
+    print("+ for addition")
+    print("- for subtraction")
+    print("* for multiplication")
+    print("/ for division")
+
+    operation = input("Choose your operation: ")
+
+    if operation == "+":
+        print("Answer =", add(num1, num2))
+
+    elif operation == "-":
+        print("Answer =", subtract(num1, num2))
+
+    elif operation == "*":
+        print("Answer =", multiply(num1, num2))
+
+    elif operation == "/":
+        print("Answer =", divide(num1, num2))
+
+    else:
+      print("Invalid operation entered.")
+
+except ValueError:
+    print("Value Error: Please enter numbers only.")
+
+except ZeroDivisionError:
+    print("Zero Division Error: Cannot divide by zero.")
